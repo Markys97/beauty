cce6d27063553febcf4493d1251f3d30d2b3e8fe
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-02-22 15:10:32 +0300

    add menu burger

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 let burgerButton= document.querySelector('.header__burger');
 let menu=document.querySelector('.header__menu');
+let index=document.querySelector('.page');
 
 
 burgerButton.addEventListener('click',e=>{
     menu.classList.toggle('open-menu')
+    index.classList.toggle('isOpen')
+    
 })
+
 
